c5770f749b6becdaacb21a6be746759b9f0f5dc9
Yashwanth A <123303508+yashwanth2706@users.noreply.github.com>
2023-12-02 23:04:02 +0530

    Update VigenereCipher.js

--- VigenereCipher.js
+++ VigenereCipher.js
@@ -1,17 +1,17 @@
 function encode(message, key) {
   let alpha = 'abcdefghijklmnopqrstuvwxyz';
-  let copy, newkey;
+  let copy = "", newkey = "";
   for(k=0;k<message.length;k++)
   {
     copy=key[k%key.length]
     newkey=newkey+copy;
   }
-  let store = '';
+  let store = "";
   
   for (let i = 0; i < message.length; i++) {
     let searchc = alpha.indexOf(message[i]);
     let searchr = alpha.indexOf(newkey[i]);
-    let encrypt = (searchr !== -1 && searchc !== -1) ? searchr + searchc : 99;
+    let encrypt = (searchr !== -1 && searchc !== -1) ? (searchr + searchc) : 99;
     let crypt = (encrypt >= 26 && encrypt < 52) ? alpha[encrypt - 26] : (encrypt < 26) ? alpha[encrypt] : message[i];
     store += crypt;
   }
