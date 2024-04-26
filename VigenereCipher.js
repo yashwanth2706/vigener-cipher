3e98cc31e1a126d47a6299708178900e444532e2
yashwanth2706 <yash1anth@gmail.com>
2024-04-26 15:50:27 +0530

    modified function names and argument names for readability

--- VigenereCipher.js
+++ VigenereCipher.js
@@ -1,8 +1,17 @@
 let alpha = 'abcdefghijklmnopqrstuvwxyz';
-let cipher_msg = "", new_key = "", cipher = "", encrypt_msg = "";
-let searchc = 0, searchr = 0, encrypt = 0;
 
-function cipher_optimize_encrypt(message, key) {
+let cipher_msg = "";
+let NEW_KEY = "";
+let cipher = ""; 
+let encrypt_msg = "";
+
+let searchc = 0;
+let searchr = 0;
+let encrypt = 0;
+
+//Function to check if message length is too long
+
+function ENCRYPT_MSG(message, key) {
     if(message.length == key.length) {
         return key;
     }
@@ -11,20 +20,24 @@ function cipher_optimize_encrypt(message, key) {
         console.error("ERROR: string too long.!\n", null);
     }
     else
-    new_key = newkey(key, message.length, 0);
-    return encrypt_msg = encode(message, new_key, 0);
+    NEW_KEY = GEN_NEWKEY(key, message.length, 0);
+    return ENCRYPT_MSG = ENCODE(message, NEW_KEY, 0);
 }
 
-function newkey(key, msg_length, k) {
+//Function to generate new key
+
+function GEN_NEWKEY(key, msg_length, k) {
     if(msg_length == 0) {
-        return new_key;
+        return NEW_KEY;
     }
     else
-    new_key = new_key + key[k%(key.length)];
-    return newkey(key, msg_length-1, k+1);
+    NEW_KEY = NEW_KEY + key[k%(key.length)];
+    return GEN_NEWKEY(key, msg_length-1, k+1);
 }
 
-function encode(message, msg_newkey, i) {
+//Function to Encrypt message to Vigenere Cipher
+
+function ENCODE(message, msg_newkey, i) {
     if( i == message.length) {
         return cipher_msg;
     }
@@ -34,7 +47,7 @@ function encode(message, msg_newkey, i) {
     encrypt = (searchc !== -1) && (searchr !== -1) ? (searchr + searchc) : 99;
     cipher = (encrypt >= 26 && encrypt < 52) ? alpha[encrypt - 26] : (encrypt < 26) ? alpha[encrypt] : message[i];
     cipher_msg = cipher_msg + cipher;
-    return encode(message, msg_newkey, i+1);
+    return ENCODE(message, msg_newkey, i+1);
 }
 
-console.log(cipher_optimize_encrypt("attack at dawn", "secret"));
+console.log(ENCRYPT_MSG("attack at dawn", "secret"));
