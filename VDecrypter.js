fb40e9f304b285e1357ddb386965139bb6bf695e
yashwanth2706 <yash1anth@gmail.com>
2024-04-26 15:31:00 +0530

    Added comments, modified function argument names for readability

--- VDecrypter.js
+++ VDecrypter.js
@@ -1,10 +1,18 @@
 let alpha = "abcdefghijklmnopqrstuvwxyz";
-let decode_msg = "", decipher = "", new_key = "";
-let searchc = 0, searchr = 0, decrypt = 0;
 
-function checkmsg_len(crptmsg, key) {
+let decode_msg = ""; 
+let decipher = "";
+let new_key = "";
+
+let searchc = 0;
+let searchr = 0;
+let decrypt = 0;
+
+//Function to check if the length of message is too long
+
+function DECODE_MSG(crptmsg, key) {
     if(crptmsg.length > 5000 || key.length > 5000) {
-        console.error("error string too long.!", null);
+        console.error("Error string too long.!", null);
         return -1;
     }
     else
@@ -12,34 +20,40 @@ function checkmsg_len(crptmsg, key) {
         return key;
     }
     else{
-        let generatekey = decode_newkey(key, crptmsg.length, 0);
-        return decode(crptmsg, generatekey, 0);
+        let generatedKey = newkey(key, crptmsg.length, 0);
+        return decode(crptmsg, generatedKey, 0);
     }
 }
 
-function decode_newkey(key, msg_length, k) {
+//Recursive Function to generate new key if key length is short than message or longer than message
+//newkey function generates the key length exactly equal to meaage length
+
+function newkey(key, msg_length, k) {
     if(msg_length == 0) {
         return new_key;
     }
     else
     new_key = new_key + key[k%(key.length)];
-    return decode_newkey(key, msg_length-1, k+1);
+    return newkey(key, msg_length-1, k+1);
 }
 
+//Recursive function to impelement decoding for Vigenere cipher 
 
-function decode(crptmsg, msg_newkey, d) {
+function decode(crptmsg, NEW_KEY, d) {
     if(d == crptmsg.length) {
         return decode_msg;
     }
     else
     searchc = alpha.indexOf(crptmsg[d]);
-    searchr = alpha.indexOf(msg_newkey[d]);
-    decrypt = ((searchr == -1) || (searchc == -1)) ? 99 : (searchc-searchr);
+    searchr = alpha.indexOf(NEW_KEY[d]);
+    decrypt = ((searchr == -1) || (searchc == -1)) ? 100 : (searchc-searchr);
     decrypt = (decrypt < 0) ? decrypt + 26 : decrypt;
     decipher = (decrypt > 26 && decrypt <= 52) ? alpha[decrypt-26] : (decrypt >= 0 && decrypt < 26) ? alpha[decrypt] : crptmsg[d];
     decode_msg = decode_msg + decipher;
-    return decode(crptmsg, msg_newkey, d+1)
+    return decode(crptmsg, NEW_KEY, d+1)
 }
 
-let decrypt_message = checkmsg_len("sxvrgd ev htor","secret");
+//Function call
+
+let decrypt_message = DECODE_MSG("sxvrgd ev htor","secret");
 console.log(decrypt_message);
